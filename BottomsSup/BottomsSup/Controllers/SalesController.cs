26fa0449f473729a4fe1a18364ae1b9e489a80b5
Charles Erickson <cse621@gmail.com>
2019-05-22 14:21:51 -0500

    Added sales controller

---
+++ BottomsSup/BottomsSup/Controllers/SalesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BottomsSup.Models;
+using Microsoft.AspNet.Identity;
+
+namespace BottomsSup.Controllers
+{
+    public class SalesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var sales = db.Sales.Include(s => s.Bar);
+            return View(sales.ToList());
+        }
+
+        // GET: Sales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sales);
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            ViewBag.BarId = new SelectList(db.Bars, "BarId", "BarName");
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SalesId,DateOfSales,TotalSales,TotalLabor,BarId")] Sales sales)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId= User.Identity.GetUserId();
+                sales.BarId = db.Bars.Where(b => b.ApplicationUserId == userId).Select(j => j.BarId).FirstOrDefault();
+                //sales.TotalLabor= Convert.ToDouble(sales.TotalLabor);
+                var dailySales = Convert.ToDouble(sales.TotalSales);
+                var dailyLabor = Convert.ToDouble(sales.TotalLabor);
+                sales.LaborPercentage = dailyLabor / dailySales;
+                db.Sales.Add(sales);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BarId = new SelectList(db.Bars, "BarId", "BarName", sales.BarId);
+            return View(sales);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BarId = new SelectList(db.Bars, "BarId", "BarName", sales.BarId);
+            return View(sales);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SalesId,DateOfSales,TotalSales,TotalLabor,LaborPercentage,BarId")] Sales sales)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sales).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BarId = new SelectList(db.Bars, "BarId", "BarName", sales.BarId);
+            return View(sales);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sales);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sales sales = db.Sales.Find(id);
+            db.Sales.Remove(sales);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
