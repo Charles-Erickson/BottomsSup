b9f15b024cfe5afa4949c842b6b1d5d72c561aa0
Charles Erickson <cse621@gmail.com>
2019-05-31 08:25:57 -0500

    got a chart to appear

--- BottomsSup/BottomsSup/Models/Sales.cs
+++ BottomsSup/BottomsSup/Models/Sales.cs
@@ -31,3 +31,117 @@ namespace BottomsSup.Models
         public Bar Bar { get; set; }
     }
 }
+
+
+
+
+
+//<head>
+//    <script src = "~/Scripts/Chart.js" ></ script >
+//    < script src="~/Scripts/Chart.min.js"></script>
+//    <script src = "~/Scripts/bootstrap.min.js" ></ script >
+//    < script src="~/Scripts/jquery-3.3.1.min.js"></script>
+//    <link href = "~/Content/bootstrap.min.css" rel="stylesheet" />
+//    <style>
+//        #chart_container {
+//            width: 800px;
+//            height: 400px;
+//            border: 1px solid #000000;
+//            padding: 1px;
+//            border-radius: 4px;
+//        }
+//    </style>
+
+//</head>
+
+
+//<body>
+
+
+//    <div id = "chart_container" >
+//        < canvas id="bar_chart"></canvas>
+
+
+//    </div>
+
+//    <script>
+//        var totals = ViewBag.Total;
+//var dates = ViewBag.dates;
+//var ctx = $("#bar_chart");
+//        var myChart = new Chart(ctx, {
+//            type: 'line',
+//            data: {
+//                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//                datasets: [{
+//                    label: '# of Votes',
+//                    data: [12, 19, 3, 5, 2, 3],
+//                    backgroundColor: [
+//                        'rgba(255, 99, 132, 0.2)',
+//                        'rgba(54, 162, 235, 0.2)',
+//                        'rgba(255, 206, 86, 0.2)',
+//                        'rgba(75, 192, 192, 0.2)',
+//                        'rgba(153, 102, 255, 0.2)',
+//                        'rgba(255, 159, 64, 0.2)'
+//                    ],
+//                    borderColor: [
+//                        'rgba(255, 99, 132, 1)',
+//                        'rgba(54, 162, 235, 1)',
+//                        'rgba(255, 206, 86, 1)',
+//                        'rgba(75, 192, 192, 1)',
+//                        'rgba(153, 102, 255, 1)',
+//                        'rgba(255, 159, 64, 1)'
+//                    ],
+//                    borderWidth: 1
+//                }]
+//            },
+//            options: {
+//                scales: {
+//                    yAxes: [{
+//                        ticks: {
+//                            beginAtZero: true
+//                        }
+//                    }]
+//                }
+//            }
+//    </script>
+
+
+
+
+
+
+
+
+
+//    @*//var barChart = new Chart(ctx, {
+//            //    type: 'line',
+//            //    data
+//            //    datasets:[{
+//            //        label: "Line Chart",
+//            //        data: [12,19,3,5,2,3]
+//            //        backgroundColor: ["blue"],
+//            //        borderWidth:1
+
+//            //    }]
+
+//            //},
+//            //    options: {
+//            //        maintainAspectRatio: false,
+//            //        scales: {
+//            //            xAxes: [{ ticks: {beginAtZero:true}}]
+//            //        },
+//            //        legend: {display:false},
+//            //    }
+
+//        </script>*@
+
+
+
+//</body>
+
+
+
+
+
+
+//</html>*@
